2ca624487aa750773b18f69ac2b4e38ea3d71943
alok <your_email@example.com>
2025-06-23 00:00:38 +0530

    basic setup

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+
+
+const App=()=>{
+  return(
+    <View>
+      <Text>App start</Text>
+    </View>
+  )
+}
+
+export default App
